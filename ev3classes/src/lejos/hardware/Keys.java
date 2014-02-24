79a51bcedd012730ea2a322adfbb94c38a44745a
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-02-24 19:57:30 +0000

    Added Key listeners
    Former-commit-id: afc7a0e4b6047805985d742672f37cae7becb811

--- ev3classes/src/lejos/hardware/Keys.java
+++ ev3classes/src/lejos/hardware/Keys.java
@@ -1,6 +1,20 @@
 package lejos.hardware;
 
 public interface Keys {
+	
+	public static final int ID_UP = 0x1;
+	public static final int ID_ENTER = 0x2;
+	public static final int ID_DOWN = 0x4;
+	public static final int ID_RIGHT = 0x8;
+	public static final int ID_LEFT = 0x10;
+	public static final int ID_ESCAPE = 0x20;
+	public static final int ID_ALL = 0x3f;
+	
+	public static int NUM_KEYS = 6;
+	
+	public static final String VOL_SETTING = "lejos.keyclick_volume";
+	public static final String LEN_SETTING = "lejos.keyclick_length";
+	public static final String FREQ_SETTING = "lejos.keyclick_frequency";
 
 	public void discardEvents();
 	
