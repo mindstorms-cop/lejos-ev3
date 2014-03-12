9ee9c5ec2161f5ac7c67ccc02fab558049f719a5
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-03-12 20:20:26 +0000

    Added upload file action
    Former-commit-id: 36b837b718ca42f4f4c5834f355a79d6f7151312

--- org.lejos.ev3.ldt/src/main/java/org/lejos/ev3/ldt/actions/UploadAction.java
+++ org.lejos.ev3.ldt/src/main/java/org/lejos/ev3/ldt/actions/UploadAction.java
@@ -1,8 +1,12 @@
 package org.lejos.ev3.ldt.actions;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.rmi.Naming;
 import java.util.ArrayList;
+
+import lejos.remote.ev3.RMIMenu;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IAction;
@@ -13,6 +17,8 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionDelegate;
 import org.eclipse.ui.progress.IProgressService;
+import org.lejos.ev3.ldt.util.BrickInfo;
+import org.lejos.ev3.ldt.util.Discover;
 import org.lejos.ev3.ldt.util.LeJOSEV3Util;
 import org.lejos.ev3.ldt.util.ToolStarter;
 
@@ -58,14 +64,28 @@ public class UploadAction extends ActionDelegate {
 				
 				ArrayList<String> args = new ArrayList<String>();
 				LeJOSEV3Util.getUploadOpts(args);
-				for (File f : fileList)
-					args.add(f.getAbsolutePath());
 				
-				int r = 0;
-				if (r == 0)
-					LeJOSEV3Util.message("files have been uploaded successfully");
-				else
-					LeJOSEV3Util.error("uploading "+fileList+" failed with exit status "+r);
+				BrickInfo[] bricks = Discover.discover();
+				
+				if (bricks.length ==  0) {
+					LeJOSEV3Util.error("No EV3 Found");					
+				} else {
+				
+					RMIMenu menu = (RMIMenu)Naming.lookup("//" + bricks[0].getIPAddress() + "/RemoteMenu");
+	
+					for (File f : fileList) {
+						args.add(f.getAbsolutePath());
+						
+						FileInputStream in = new FileInputStream(f);
+						byte[] data = new byte[(int)f.length()];
+					    in.read(data);
+					    in.close();
+					    menu.uploadFile("/home/lejos/programs/" + f.getName(), data);
+					}
+					
+				    LeJOSEV3Util.message("Files have been uploaded successfully");
+				}
+
 			}
 			finally
 			{
