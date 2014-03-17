c34f4a7c9a6261c04f28095d65a3b78abf8777dc
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-03-17 22:48:18 +0000

    Added option to upload and run with scp and ssh
    Former-commit-id: d2e897a4425cff0e0b6f39aa797b4bd4bf6ef4fa

--- org.lejos.ev3.ldt/src/main/java/org/lejos/ev3/ldt/launch/LaunchConstants.java
+++ org.lejos.ev3.ldt/src/main/java/org/lejos/ev3/ldt/launch/LaunchConstants.java
@@ -8,10 +8,14 @@ public class LaunchConstants
 {
 	public static final String PREFIX = LeJOSEV3Plugin.ID+".";
 	
-	public static final String SUFFIX_RUN_AFTER_UPLOAD = ".runAfterUpload";;
+	public static final String SUFFIX_RUN_AFTER_UPLOAD = ".runAfterUpload";
+	public static final String SUFFIX_USE_DEFAULT = ".useDefaults";
 
 	public static final String KEY_TARGET_BRICK_NAME = PREFIX+PreferenceConstants.KEY_TARGET_BRICK_NAME;
+	public static final String KEY_TARGET_USE_DEFAULTS = PREFIX+"connectionUseDefaults";
 	
 	public static final String KEY_NORMAL_RUN_AFTER_UPLOAD = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_RUN_AFTER_UPLOAD;
+	
+	public static final String KEY_NORMAL_USE_DEFAULTS = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_USE_DEFAULT;
 
 }
