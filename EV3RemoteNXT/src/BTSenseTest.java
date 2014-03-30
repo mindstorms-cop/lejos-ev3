13635bda456c3569bb618c8a79fd6c08a4d047e5
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-03-30 14:51:04 +0100

    Make BTSense use latest data and wait for first data to arrive
    Former-commit-id: da42f6fdd32d3a5287ab13222e3397f734bfeebc

--- EV3RemoteNXT/src/BTSenseTest.java
+++ EV3RemoteNXT/src/BTSenseTest.java
@@ -1,5 +1,6 @@
 import lejos.hardware.sensor.MindsensorsBTSense;
 import lejos.hardware.sensor.SensorMode;
+import lejos.utility.Delay;
 
 
 public class BTSenseTest {
@@ -7,32 +8,40 @@ public class BTSenseTest {
 		MindsensorsBTSense btSense = new MindsensorsBTSense();
 		
 		SensorMode dateMode = btSense.getSensorMode('D');
+		System.out.println("Created data mode");
 		SensorMode accelMode = btSense.getSensorMode('A');
-		
-		Thread.sleep(5000);
+		System.out.println("Created acceleration mode");
 		
 		float[] sample = new float[dateMode.sampleSize()];
 		
 		for(int i=0;i<10;i++) {
 			dateMode.fetchSample(sample,0);
-			System.out.print("Sample is ");
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("Date sample is ");
 			
 			for(int j=0;j<dateMode.sampleSize();j++) {
-				System.out.print(((int) sample[j]) + " ");
+				sb.append(((int) sample[j]) + " ");
 			}
-			System.out.println();
+			System.out.println(sb.toString());
+			
+			Delay.msDelay(500);
 		}
 		
 		sample = new float[accelMode.sampleSize()];
 		
 		for(int i=0;i<50;i++) {
 			accelMode.fetchSample(sample,0);
-			System.out.print("Sample is ");
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("Acceleration sample is ");
 			
 			for(int j=0;j<accelMode.sampleSize();j++) {
-				System.out.print(((int) sample[j]) + " ");
+				sb.append(((int) sample[j]) + " ");
 			}
-			System.out.println();
+			System.out.println(sb.toString());
+			
+			Delay.msDelay(500);
 		} 
 	}
 }
