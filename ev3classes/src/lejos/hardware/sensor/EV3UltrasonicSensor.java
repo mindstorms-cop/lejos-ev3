0fcfb5e04e016c363b2a2a249ef3b30c79fc2225
Aswin Bouwmeester <aswin.bouwmeester@gmail.com>
2014-07-02 09:24:43 +0200

    Updated Javadoc for Lego EV3 sensors and enforced sensor:mode hierarchy

--- ev3classes/src/lejos/hardware/sensor/EV3UltrasonicSensor.java
+++ ev3classes/src/lejos/hardware/sensor/EV3UltrasonicSensor.java
@@ -6,8 +6,60 @@ import lejos.robotics.SampleProvider;
 
 /**
  * Sensor driver for the Lego EV3 Ultrasonic sensor.<br>
+ * The EV3 Ultrasonic sensor measures distance to an object in front of the sensor. It can also be used to detect other (active) Ultrasonic sensor in the vicinity.
  * 
  * @author Aswin Bouwmeester
+ * 
+ */
+/**
+ * <b>Lego EV3 Ultrasonic sensor</b><br>
+ * The EV3 Ultrasonic sensor measures distance to an object in front of the sensor. It can also be used to detect other (active) Ultrasonic sensor in the vicinity.
+ * 
+ * 
+ * 
+ * <p>
+ * <table border=1>
+ * <tr>
+ * <th colspan=4>Supported modes</th>
+ * </tr>
+ * <tr>
+ * <th>Mode name</th>
+ * <th>Description</th>
+ * <th>unit(s)</th>
+ * <th>Getter</th>
+ * </tr>
+ * <tr>
+ * <td>Distance</td>
+ * <td>Measures distance to an object in front of the sensor</td>
+ * <td>Metre</td>
+ * <td> {@link #getDistanceMode() }</td>
+ * </tr>
+ * </tr>
+ * <tr>
+ * <td>Listen</td>
+ * <td>Listens for other ultrasonic sensors</td>
+ * <td>Boolean</td>
+ * <td> {@link #getListenMode() }</td>
+ * </tr>
+ * </table>
+ * 
+ * 
+ * <p>
+ * <b>Sensor configuration</b><br>
+ * The sensor can be switched off and on using the {@link #enable} and {@link #disable} methods. Disabling the sensor also shuts down the lights.
+ * 
+ * <p>
+ * 
+ * @see <a href="http://www.ev-3.net/en/archives/844"> Sensor Product page </a>
+ * @see <a href="http://sourceforge.net/p/lejos/wiki/Sensor%20Framework/"> The
+ *      leJOS sensor framework</a>
+ * @see {@link lejos.robotics.SampleProvider leJOS conventions for
+ *      SampleProviders}
+ * 
+ *      <p>
+ * 
+ * 
+ * @author Your name
  * 
  */
 public class EV3UltrasonicSensor extends UARTSensor {
