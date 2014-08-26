ea52b5bc74b8fc3b90a16b0d831f4295cbb927c0
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-08-26 17:24:28 +0100

    Improved exception handling
    Former-commit-id: 86c597344823aa793a5db7430f97273ffaf7764f

--- ev3classes/src/lejos/remote/ev3/RMIRemoteSampleProvider.java
+++ ev3classes/src/lejos/remote/ev3/RMIRemoteSampleProvider.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import lejos.hardware.DeviceException;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.BaseSensor;
@@ -14,6 +15,7 @@ public class RMIRemoteSampleProvider extends UnicastRemoteObject implements RMIS
 	private static final long serialVersionUID = -8432755905878519147L;
 	private SampleProvider provider;
 	private BaseSensor sensor;
+	private int sampleSize;
 
 	protected RMIRemoteSampleProvider(String portName, String sensorName, String modeName) throws RemoteException {
 		super(0);
@@ -24,19 +26,18 @@ public class RMIRemoteSampleProvider extends UnicastRemoteObject implements RMIS
 			Constructor<?> con = c.getConstructor(params);
 			Object[] args = new Object[1];
 			args[0] = LocalEV3.get().getPort(portName);
-			Object obj = con.newInstance(args);
-			sensor = (BaseSensor) obj;
-			if (modeName == null) provider = (SampleProvider) obj;
+			sensor = (BaseSensor) con.newInstance(args);
+			if (modeName == null) provider = (SampleProvider) sensor;
 			else provider = sensor.getMode(modeName);
+			sampleSize = provider.sampleSize();
 		} catch (Exception e) {
-			throw new RemoteException("Failed to create sample provider", e.getCause());
+			if (sensor != null) sensor.close();
+			throw new DeviceException("Failed to create sample provider", e);
 		}
 	}
 
 	@Override
 	public float[] fetchSample() throws RemoteException {
-		if (provider == null) throw new RemoteException("Sample Provider creation failed");
-		int sampleSize = provider.sampleSize();
 		float[] sample = new float[sampleSize];
 		provider.fetchSample(sample, 0);
 		return sample;
