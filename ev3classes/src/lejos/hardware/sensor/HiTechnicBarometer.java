a8aa6a149c10cd945cb74059f4aecece7f500753
Aswin Bouwmeester <Aswin.Bouwmeester@gmail.com>
2014-08-27 23:32:00 +0200

    Imprevements to documentation and SensorMode
    Former-commit-id: 18b9fd5514deed406d9dbebdd96eab783653d724

--- ev3classes/src/lejos/hardware/sensor/HiTechnicBarometer.java
+++ ev3classes/src/lejos/hardware/sensor/HiTechnicBarometer.java
@@ -3,8 +3,6 @@ package lejos.hardware.sensor;
 import lejos.hardware.port.I2CPort;
 import lejos.hardware.port.Port;
 
-
-
 /**
  * <b>Hitechnic Barometric sensor</b><br>
  * The sensor measures both atmospheric pressure and temperature.
@@ -45,7 +43,9 @@ import lejos.hardware.port.Port;
  * The sensor can be calibrated for pressure using the calibrate method.
  * <p>
  * 
- * @see <a href="http://www.hitechnic.com/cgi-bin/commerce.cgi?preadd=action&key=NBR1036"> Sensor Product page </a>
+ * @see <a
+ *      href="http://www.hitechnic.com/cgi-bin/commerce.cgi?preadd=action&key=NBR1036">
+ *      Sensor Product page </a>
  * @see <a href="http://sourceforge.net/p/lejos/wiki/Sensor%20Framework/"> The
  *      leJOS sensor framework</a>
  * @see {@link lejos.robotics.SampleProvider leJOS conventions for
@@ -54,123 +54,124 @@ import lejos.hardware.port.Port;
  *      <p>
  * 
  * 
- * @author  Matthias Paul Scholz
+ * @author Matthias Paul Scholz
  * 
  */
 public class HiTechnicBarometer extends I2CSensor {
 
-	private static final int BAROMETRIC_TEMPERATURE = 0x42;
-	private static final int BAROMETRIC_PRESSURE = 0x44;
-	private static final int BAROMETRIC_PRESSURE_CALIBRATION = 0x46;
-	private final double INHG_TO_HPA = 2992 / 1013.25;
-	private final float STANDARD_ATMOSPHERIC_PRESSURE = 1013.25f;
+  private static final int BAROMETRIC_TEMPERATURE          = 0x42;
+  private static final int BAROMETRIC_PRESSURE             = 0x44;
+  private static final int BAROMETRIC_PRESSURE_CALIBRATION = 0x46;
+  private final double     INHG_TO_HPA                     = 2992 / 1013.25;
+  private final float      STANDARD_ATMOSPHERIC_PRESSURE   = 1013.25f;
 
-	private final byte[] buffer = new byte[2];
+  private final byte[]     buffer                          = new byte[2];
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param port the {@link I2CPort} the sensor is connected to.
-	 */
-	public HiTechnicBarometer(final I2CPort port) {
-		super(port, DEFAULT_I2C_ADDRESS);
-	}
+  /**
+   * Constructor.
+   * 
+   * @param port
+   *          the {@link I2CPort} the sensor is connected to.
+   */
+  public HiTechnicBarometer(final I2CPort port) {
+    super(port, DEFAULT_I2C_ADDRESS);
+  }
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param port the {@link I2CPort} the sensor is connected to.
-	 * @param address the address
-	 */
-	public HiTechnicBarometer(final I2CPort port, final int address) {
-		super(port, address);
-		init();
-	}
+  /**
+   * Constructor.
+   * 
+   * @param port
+   *          the {@link I2CPort} the sensor is connected to.
+   * @param address
+   *          the address
+   */
+  public HiTechnicBarometer(final I2CPort port, final int address) {
+    super(port, address);
+    init();
+  }
 
-	public HiTechnicBarometer(final Port port, final int address) {
-	        super(port, address, TYPE_LOWSPEED);
-	        init();
-	}
+  public HiTechnicBarometer(final Port port, final int address) {
+    super(port, address, TYPE_LOWSPEED);
+    init();
+  }
 
-    public HiTechnicBarometer(final Port port) {
-        this(port, DEFAULT_I2C_ADDRESS);
-        init();
+  public HiTechnicBarometer(final Port port) {
+    this(port, DEFAULT_I2C_ADDRESS);
+    init();
+  }
+
+  protected void init() {
+    setModes(new SensorMode[] { new PressureMode(), new TemperatureMode() });
+    if (getCalibrationMetric() == 0)
+      calibrate(STANDARD_ATMOSPHERIC_PRESSURE);
+  }
+
+  /**
+   * Re-calibrates the sensor.
+   * 
+   * @param pascals
+   *          the calibration value in hectopascals
+   */
+  public void calibrate(float pascals) {
+    int calibrationImperial = (int) ((pascals / 10) / INHG_TO_HPA);
+    buffer[0] = (byte) ((calibrationImperial & 0xff00) >> 8);
+    buffer[1] = (byte) (calibrationImperial & 0x00ff);
+    sendData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2);
+  }
+
+  /**
+   * @return the present calibration value in pascals. Will be 0 in case no
+   *         explicit calibration has been performed.
+   */
+  public float getCalibrationMetric() {
+    getData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2);
+    int result = ((buffer[0] & 0xff) << 8) + buffer[1];
+    return (float) ((result / INHG_TO_HPA) * 10);
+  }
+
+  public SensorMode getPressureMode() {
+    return getMode(0);
+  }
+
+  private class PressureMode implements SensorMode {
+
+    @Override
+    public int sampleSize() {
+      return 1;
     }
-    
-    protected void init() {
-    	setModes(new SensorMode[]{ new PressureMode(), new TemperatureMode() });
-    	if (getCalibrationMetric()==0) calibrate(STANDARD_ATMOSPHERIC_PRESSURE);
+
+    @Override
+    public String getName() {
+      return "Pressure";
     }
 
-	/**
-	 * Re-calibrates the sensor.
-	 * 
-	 * @param pascals the calibration value in hectopascals
-	 */
-	public void calibrate(float pascals ) {
-		int calibrationImperial = (int) ((pascals / 10) / INHG_TO_HPA); 
-		buffer[0] = (byte) ((calibrationImperial & 0xff00) >> 8);
-		buffer[1] = (byte) (calibrationImperial & 0x00ff);
-		sendData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2);
-	}
+    @Override
+    public void fetchSample(float[] sample, int offset) {
+      getData(BAROMETRIC_PRESSURE, buffer, 2);
+      sample[0] = (float) (((((buffer[0] & 0xff) << 8) + buffer[1]) / INHG_TO_HPA) * 10);
+    }
 
-	/**
-	 * @return the present calibration value in pascals.
-	 *         Will be 0 in case no explicit calibration has been performed.
-	 */
-	public float getCalibrationMetric() {
-		getData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2);
-		int result = ((buffer[0] & 0xff) << 8) + buffer[1];
-		return (float) ((result / INHG_TO_HPA) * 10);
-	}
-	
+  }
 
-	public SensorMode getPressureMode() {
-		return getMode(0);
-	}
+  public SensorMode getTemperatureMode() {
+    return getMode(1);
+  }
 
-	private class PressureMode implements SensorMode {
+  private class TemperatureMode implements SensorMode {
+    @Override
+    public int sampleSize() {
+      return 1;
+    }
 
-	@Override
-	public int sampleSize() {
-		return 1;
-	}
-	
-	@Override
-	public String getName() {
-		return "Pressure";
-	}
-	
-	
+    @Override
+    public void fetchSample(float[] sample, int offset) {
+      getData(BAROMETRIC_TEMPERATURE, buffer, 2);
+      sample[offset] = (float) (((buffer[0] << 2) | (buffer[1] & 0xFF)) * 10 + 273.15);
+    }
 
-	@Override
-	public void fetchSample(float[] sample, int offset) {
-		getData(BAROMETRIC_PRESSURE, buffer, 2);
-		sample[0] = (float) (((((buffer[0] & 0xff) << 8) + buffer[1]) / INHG_TO_HPA) * 10);
-	}
-	
-	}
-
-	
-	public SensorMode getTemperatureMode() {
-		return getMode(1);
-	}
-	
-	private class TemperatureMode implements SensorMode {
-		@Override
-		public int sampleSize() {
-			return 1;
-		}
-
-		@Override
-		public void fetchSample(float[] sample, int offset) {
-			getData(BAROMETRIC_TEMPERATURE, buffer, 2);
-		    sample[offset] =  (float) (((buffer[0] << 2) | (buffer[1] & 0xFF)) * 10 + 273.15);	
-		}
-
-		@Override
-		public String getName() {
-			return "Temperature";
-		}	
-	}
+    @Override
+    public String getName() {
+      return "Temperature";
+    }
+  }
 }
