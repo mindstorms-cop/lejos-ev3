c4f39df6d333c55734654fa1c9ab623965c54f0f
Aswin Bouwmeester <Aswin Bouwmeester@Suite909>
2015-05-01 16:31:45 +0200

    Corrected method names in JavaDoc

--- ev3classes/src/lejos/robotics/chassis/Chassis.java
+++ ev3classes/src/lejos/robotics/chassis/Chassis.java
@@ -12,9 +12,9 @@ import lejos.utility.Matrix;
  * The primary goal of the chassis is to make a robot move. 
  * This can be done in two different ways. <br>
  * One way is by specifying the speed at which a chassis should move, this is called velocity mode. 
- * The primary methods for this mode are {@link #setVelocity(double, double) travel} and {@link #stop() stop} .<br>
+ * The primary methods for this mode are {@link #setVelocity(double, double) setVelocity} and {@link #stop() stop} .<br>
  * The second way is by specifying  the kind of move the chassis should make, this is called move mode. 
- * The primary methods for this mode are {@link #arc(double, double) arc}, {@link #rotate(double) rotateTo} and {@link #travel(double) move}.
+ * The primary methods for this mode are {@link #arc(double, double) arc}, {@link #rotate(double) rotate} and {@link #travel(double) travel}.
  * The move mode methods use speed and acceleration parameters that have to be specified beforehand using {@link #setSpeed(double, double) setSpeed} 
  * and {@link #setAcceleration(double, double) setAcceleration}. <br>
  * Depending on the application one of these modes will suit better, but the modes can be used together.</p>
@@ -33,18 +33,18 @@ import lejos.utility.Matrix;
  * The chassis makes sure the forward speed will be such that the angular speed will not exceed the specified setting. <p>
  * 
  * <p><b>Speed transitions</b><br>
- * To prevent jerky movements of the robot the velocity based methods {@link #setVelocity(double, double) travel} and {@link #stop() stop} 
+ * To prevent jerky movements of the robot the velocity based methods {@link #setVelocity(double, double) setVelocity} and {@link #stop() stop} 
  * ensure smooth speed transitions using the acceleration settings that are specified using the 
  * {@link #setAcceleration(double, double) setAcceleration} method. 
  * This means that each of the wheels of the chassis takes exactly the same time to reach final speed, 
  * no matter what the speed of the wheels was at the moment the travel method was issued.<br>
- * The move beased methods do not know these smooth transitions. 
+ * The move based methods do not know these smooth transitions. 
  * These methods assume that the robot is at stand still when the method is issued and that the robot is at stand still again when the move ends. 
  * (Without this assumption it cannot be guaranteed that the trajectory of the robot has indeed the same shape as the method name implies.) 
  * 
  * <p><b>Odometry</b><br>
  * The chassis can provide a {@link lejos.robotics.localization.PoseProvider PoseProvider} that keeps track of the robots pose using the encoders of the wheels.
- * The object is provided by the {@link #getPoseProvider() getOdometer} method.</p>
+ * The object is provided by the {@link #getPoseProvider() getPoseProvider} method.</p>
  * 
  * @author Aswin Bouwmeester
  *
@@ -57,43 +57,43 @@ public interface Chassis {
    */
   public double getLinearSpeed();
   
-  /** Sets the linear speed as is used in moveTo, Arc, and Rotate methods
+  /** Sets the linear speed as is used in travel, arc, and rotate methods
    * @param linearSpeed
    * Linear speed in robot units/second
    */
   public void setLinearSpeed(double linearSpeed);
   
-  /** Gets the setting for angular speed as is used in moveTo, Arc, and Rotate methods
+  /** Gets the setting for angular speed as is used in travel, arc, and rotate methods
    * @return
    * Angular speed in degrees/second.
    */
   public double getAngularSpeed();
   
-  /** Sets the angular speed as is used in moveTo, Arc, and Rotate methods
+  /** Sets the angular speed as is used in travel, arc, and rotate methods
    * @param angularSpeed
    * Angular speed in degrees/second.
    */
   public void setAngularSpeed(double angularSpeed);
   
-  /** Gets the setting for linear acceleration as is used in moveTo, Arc, Rotate and travel methods
+  /** Gets the setting for linear acceleration as is used in travel, arc, rotate and setvelocity methods
    * @return
    * Linear acceleration in robot units/second^2
    */
   public double getLinearAcceleration();
   
-  /** Sets the linear acceleration as is used in moveTo, Arc, Rotate and travel methods
+  /** Sets the linear acceleration as is used in travel, arc, rotate and setvelocity methods
    * @param linearAcceleration
    * Linear acceleration in robot units/second^2
    */
   public void setLinearAcceleration(double linearAcceleration);
   
-  /** Gets the setting for angular acceleration as is used in moveTo, Arc, Rotate and travel methods
+  /** Gets the setting for angular acceleration as is used in travel, arc, rotate and setvelocity methods
    * @return
    * Angular acceleration in degrees/second^2.
    */
   public double getAngularAcceleration();
   
-  /** Sets the angular acceleration as is used in moveTo, Arc, Rotate and travel methods
+  /** Sets the angular acceleration as is used in travel, arc, rotate and setvelocity methods
    * @param angularAcceleration
    * Angular Acceleration in degrees/second^2.
    */
@@ -106,7 +106,7 @@ public interface Chassis {
   public Matrix getCurrentSpeed();
 
   /**
-   * Returns True if the robot is moving.
+   * Returns true if the robot is moving.
    * 
    * @return
    */
@@ -136,7 +136,8 @@ public interface Chassis {
    */
   public void setVelocity(double linearSpeed, double direction, double angularSpeed);
   
-  /** Moves a holonomic robot with the specified speed
+  /** Moves a holonomic robot with the specified speed. <br>
+   * The method will throw an exception when issued on a differential chassis.
    * @param xSpeed
    * speed along the robots x-axis
    * @param ySpeed
@@ -227,7 +228,7 @@ public interface Chassis {
   public void moveStart();
   
   /** Method used by the MovePilot to update a move object that describes the move executed since the last call to startMove. <p>
-   * This method is only to be used by applications that only apply moves that meet the following conditions:<ul>
+   * This method is only to be used by applications that apply just moves that meet the following conditions:<ul>
    * <li> The move must start and end with the robot being motionless</li>
    * <li> The speed ratio between the wheels must be constant during the move</li>
    * </ul>
