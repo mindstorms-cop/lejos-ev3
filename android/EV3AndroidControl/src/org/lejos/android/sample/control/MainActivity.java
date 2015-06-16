ac510371581b8b1bd81947e0a7b4d8103b7c9605
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2015-06-16 13:24:30 +0100

    Use async task for network access. app is a bit buggy.

--- android/EV3AndroidControl/src/org/lejos/android/sample/control/MainActivity.java
+++ android/EV3AndroidControl/src/org/lejos/android/sample/control/MainActivity.java
@@ -1,13 +1,20 @@
 package org.lejos.android.sample.control;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import lejos.hardware.Audio;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.remote.ev3.RemoteRequestEV3;
+import lejos.remote.ev3.RemoteRequestSampleProvider;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
 import android.os.AsyncTask;
+import android.os.AsyncTask.Status;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -22,6 +29,10 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Button connect;
 	private Audio audio;
 	private TextLCD lcd;
+	private Exception ex;
+	private RemoteRequestSampleProvider sp;
+	private AsyncTask<String, Integer, Long> task;
+	private float distance;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,22 +62,46 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		if (v.getId() == R.id.left) new Control().execute("rotate left");
-		else if (v.getId() == R.id.right) new Control().execute("rotate right");
-		else if (v.getId() == R.id.stop) new Control().execute("stop");
-		else if (v.getId() == R.id.forward) new Control().execute("forward");
-		else if (v.getId() == R.id.backward) new Control().execute("backward");
+		
+		if (v.getId() == R.id.left) task = new Control().execute("rotate left");
+		else if (v.getId() == R.id.right) task = new Control().execute("rotate right");
+		else if (v.getId() == R.id.stop) task = new Control().execute("stop");
+		else if (v.getId() == R.id.forward) task = new Control().execute("forward");
+		else if (v.getId() == R.id.backward) task = new Control().execute("backward");
 		else if (v.getId() == R.id.connect) {
 			if (ev3 == null) {
-				new Control().execute("connect","192.168.0.9");
+				task = new Control().execute("connect","192.168.0.51");
 				connect.setText("Disconnect");
+				timerHandler.postDelayed(timerRunnable, 5000);
 			}
 			else {
 				new Control().execute("disconnect");
 				connect.setText("Connect");
 			}
 		}
+		
+		try {
+			task.get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+        	if (ev3 == null) return;
+        	new Control().execute("touch");
+            timerHandler.postDelayed(this, 1000);
+        }
+    };
 	
 	private class Control extends AsyncTask<String, Integer, Long> {
 		 
@@ -80,12 +115,15 @@ public class MainActivity extends Activity implements OnClickListener {
 					audio = ev3.getAudio();
 					lcd = ev3.getTextLCD();
 					audio.systemSound(3);
+					sp = (RemoteRequestSampleProvider) ev3.createSampleProvider("S1", "lejos.hardware.sensor.EV3UltrasonicSensor", "Distance");
 					return 0l;
-				} catch (IOException e) {
+				} catch (Exception e) {
+					ex = e;
 					return 1l;
 				}
 			 } else if (cmd[0].equals("disconnect") && ev3 != null) {
 				 audio.systemSound(2);
+				 if (sp != null) sp.close();
 				 left.close();
 				 right.close();
 				 ev3.disConnect();
@@ -95,7 +133,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			 
 			 if (ev3 == null) return 2l;
 			 
-			 ev3.getAudio().systemSound(1);
+			 //ev3.getAudio().systemSound(1);
 			 
 			 if (cmd[0].equals("stop")) {
 				 lcd.clear();
@@ -121,14 +159,29 @@ public class MainActivity extends Activity implements OnClickListener {
 				 lcd.drawString("Right", 4, 3);
 				 left.forward();
 				 right.backward();
+			 } else if (cmd[0].equals("touch")) {
+				 try {
+					 float[] sample = new float[1];
+					 sp.fetchSample(sample, 0);
+					 distance = sample[0];
+					 if (distance < 0.3) {
+						 left.stop();
+						 right.stop();
+						 return 03l;
+					 }
+				 } catch (Exception e) {
+					 ex = e;
+					 return 01l;
+				 }
 			 }
 			 
 			 return 0l;
 		 }
 		 
 	     protected void onPostExecute(Long result) {
-	    	 if (result == 1l) Toast.makeText(MainActivity.this, "Could not connect to EV3", Toast.LENGTH_LONG).show();
+	    	 if (result == 1l) Toast.makeText(MainActivity.this, "Could not connect to EV3: " + ex, Toast.LENGTH_LONG).show();
 	    	 else if (result == 2l) Toast.makeText(MainActivity.this, "Not connected", Toast.LENGTH_LONG).show();
+	    	 else if (result == 3l) Toast.makeText(MainActivity.this, "Distance: " + distance, Toast.LENGTH_LONG).show();
 	     }
 	}
 
